dd570b6b384b2bdbbfcf60900dd7c372a661434b
Vladimir Levin <vladimirlevinozinki@gmail.com>
2020-12-19 18:44:45 +0300

    feat: formatTime implementation

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,7 +1,129 @@
-import process from '../index';
+import formatTime from '../index';
 
-describe('Test them all', () => {
-    test('should return value', () => {
-        expect(process()).toEqual(2);
+describe('formatTime', () => {
+    describe('# Simple 24 hours format', () => {
+        const testTable = [
+            { input: 9, output: '9:00' },
+            { input: 0, output: '0:00' },
+            { input: 12.5, output: '12:30' },
+            { input: 23, output: '23:00' },
+            { input: 11, output: '11:00' },
+            { input: 6.25, output: '6:15' },
+            { input: 19.3, output: '19:18' }
+        ];
+
+        testTable.forEach(({ input, output }) => {
+            test(`should format number ${input} as "${output}"`, () => {
+                expect(formatTime(input)).toEqual(output);
+            });
+        });
+    });
+
+    describe('# Full size 12 hours format', () => {
+        const testTable = [
+            { input: 9, output: '09:00' },
+            { input: 0, output: '00:00' },
+            { input: 1.5, output: '01:30' },
+            { input: 3.75, output: '03:45' },
+            { input: 11, output: '11:00' },
+            { input: 6.25, output: '06:15' },
+            { input: 9.3, output: '09:18' }
+        ];
+
+        testTable.forEach(({ input, output }) => {
+            test(`should format number ${input} as "${output}"`, () => {
+                expect(formatTime(input, { fullSize: true })).toEqual(output);
+            });
+        });
+    });
+
+    describe('# Simple 12 hours format', () => {
+        const testTable = [
+            { input: 19, output: '7:00' },
+            { input: 20, output: '8:00' },
+            { input: 12.5, output: '12:30' },
+            { input: 23, output: '11:00' },
+            { input: 15.4, output: '3:24' },
+            { input: 6.25, output: '6:15' },
+            { input: 19.3, output: '7:18' }
+        ];
+
+        testTable.forEach(({ input, output }) => {
+            test(`should format number ${input} as "${output}"`, () => {
+                expect(formatTime(input, { timeFormat: '12h' })).toEqual(output);
+            });
+        });
+    });
+
+    describe('# Full size 12 hours format', () => {
+        const testTable = [
+            { input: 19, output: '7.00' },
+            { input: 20, output: '8.00' },
+            { input: 12.5, output: '12.30' },
+            { input: 23, output: '11.00' },
+            { input: 20, output: '8.00' },
+            { input: 12.5, output: '12.30' },
+            { input: 23, output: '11.00' },
+            { input: 15.4, output: '3.24' },
+            { input: 6.25, output: '6.15' },
+            { input: 19.3, output: '7.18' },
+            { input: 22.1, output: '10.06' }
+        ];
+
+        testTable.forEach(({ input, output }) => {
+            test(`should format number ${input} as "${output}"`, () => {
+                expect(formatTime(input, { timeFormat: '12h', divider: '.' })).toEqual(output);
+            });
+        });
+    });
+
+    describe('# Format with custom divider', () => {
+        const testTable = [
+            { input: 19, output: '07:00' },
+            { input: 20, output: '08:00' },
+            { input: 12.5, output: '12:30' },
+            { input: 23, output: '11:00' },
+            { input: 15.4, output: '03:24' },
+            { input: 6.25, output: '06:15' },
+            { input: 19.3, output: '07:18' },
+            { input: 22.1, output: '10:06' }
+        ];
+
+        testTable.forEach(({ input, output }) => {
+            test(`should format number ${input} as "${output}"`, () => {
+                expect(formatTime(input, { timeFormat: '12h', fullSize: true })).toEqual(output);
+            });
+        });
+    });
+
+    describe('# Format while removing overflow', () => {
+        const testTable = [
+            { input: 29, output: '5:00' },
+            { input: 20, output: '20:00' },
+            { input: 22.5, output: '22:30' },
+            { input: 43, output: '19:00' },
+            { input: 41, output: '17:00' },
+            { input: 66.25, output: '18:15' },
+            { input: 29.3, output: '5:18' },
+            { input: 24, output: '0:00' },
+            { input: 23.99, output: '23:59' }
+        ];
+
+        testTable.forEach(({ input, output }) => {
+            test(`should format number ${input} as "${output}"`, () => {
+                expect(formatTime(input, { removeOverflow: true })).toEqual(output);
+            });
+        });
+    });
+
+    describe('# Format with Date input as well', () => {
+        test('should format Date properly', () => {
+            const date = new Date('March 13, 08 14:20');
+
+            expect(formatTime(date)).toEqual('14:20');
+            expect(formatTime(date, { timeFormat: '12h' })).toEqual('2:20');
+            expect(formatTime(date, { timeFormat: '12h', fullSize: true })).toEqual('02:20');
+            expect(formatTime(date, { timeFormat: '12h', fullSize: true, divider: '.' })).toEqual('02.20');
+        });
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,59 @@
-export default () => {
-    return 2;
+const MAX_HOUR_VALUE = 24;
+const HALF_OF_DAY = 12;
+const MINUTES_IN_HOUR = 60;
+
+export type TimeFormat = '12h' | '24h';
+
+export type Options = {
+    timeFormat?: TimeFormat;
+    divider?: string;
+    fullSize?: boolean;
+    removeOverflow?: boolean;
 };
+
+/**
+ * @description Format hours and minutes
+ * @param {number|Date} input Number from 0 to 23.99
+ * @param {Options} options
+ * @param {TimeFormat} [options.timeFormat = '24h'] Output time format. Available values: `'12h' | '24h'`
+ * @param {string} [options.divider = ':'] Custom divider between hours and minutes
+ * @param {boolean} [options.fullSize = false] If set, will add leading zero to hours
+ * @param {boolean} [options.removeOverflow = false] If set, will decrease hours which is more than 23.99
+ */
+const formatTime = (input: number | Date, options?: Options): string => {
+    const { timeFormat = '24h', fullSize = false, divider = ':', removeOverflow = false } = options ?? {};
+
+    let hours: number;
+    let minutes: number;
+
+    if (input instanceof Date) {
+        hours = input.getHours();
+        minutes = input.getMinutes();
+    } else {
+        let base = Math.floor(input);
+        let rest = input % 1;
+
+        if (removeOverflow && base >= MAX_HOUR_VALUE) {
+            const overflowCount = Math.floor(base / MAX_HOUR_VALUE);
+            base = base - overflowCount * MAX_HOUR_VALUE;
+        }
+
+        if (rest > 0) {
+            rest = Math.floor(MINUTES_IN_HOUR * rest);
+        }
+
+        hours = base;
+        minutes = rest;
+    }
+
+    if (timeFormat === '12h' && hours > HALF_OF_DAY) {
+        hours = hours - HALF_OF_DAY;
+    }
+
+    const prefix = fullSize && hours < 10 ? '0' : '';
+    const restStr = minutes < 10 ? `0${minutes}` : `${minutes}`;
+
+    return `${prefix}${hours}${divider}${restStr}`;
+};
+
+export default formatTime;
