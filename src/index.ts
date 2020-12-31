67a95e3a9b6b4fe1edd2251c2d3d80653ef05cb2
Vladimir Levin <vladimirlevinozinki@gmail.com>
2020-12-31 14:21:51 +0300

    feat: ParseTime implementation

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ export type Options = {
  * @description Format hours and minutes
  * @param {number|Date} input Number from 0 to 23.99
  * @param {Options} options
- * @param {TimeFormat} [options.timeFormat = '24h'] Output time format. Available values: `'12h' | '24h'`
+ * @param {TimeFormat} [options.timeFormat = '24h'] Output time format. Available values: `'12h' | '24h' | 'AM-PM'`
  * @param {string} [options.divider = ':'] Custom divider between hours and minutes
  * @param {boolean} [options.fullSize = false] If set, will add leading zero to hours
  * @param {boolean} [options.removeOverflow = false] If set, will decrease hours which is more than 23.99
@@ -72,4 +72,59 @@ const formatTime = (input: number | Date, options?: Options): string => {
     return `${prefix}${hours}${divider}${restStr}${suffix}`;
 };
 
+/**
+ * @description Parse formatted hours
+ * @param {string} inputStr String that my contains hours
+ * @param {TimeFormat} [timeFormat = '24h'] Possible format of hours in input string. Available values: `'12h' | '24h' | 'AM-PM'`
+ */
+const parseTime = (inputStr: string, timeFormat: TimeFormat = '24h'): number | null => {
+    if (typeof inputStr !== 'string') {
+        return null;
+    }
+
+    const matches = inputStr.match(/([0-9]{1,2}).([0-9]{2})/i);
+
+    // [fullMatch, group1, group2]
+    if (!matches || matches.length < 3) {
+        return null;
+    }
+
+    const [, hourStr, minutesStr] = matches;
+    let hour = 0,
+        minutes = 0;
+
+    if (hourStr) {
+        hour = Number.parseInt(hourStr, 10);
+    }
+    if (minutesStr) {
+        minutes = Number.parseInt(minutesStr, 10);
+    }
+
+    switch (timeFormat) {
+        case 'AM-PM': {
+            const timeMatches = inputStr.match(/([ap].?m)/i);
+            let afterMidday = true;
+
+            if (timeMatches && timeMatches.length > 1) {
+                const [, modifier] = timeMatches;
+                afterMidday = modifier.toLowerCase().startsWith('p');
+            }
+            if (hour === HALF_OF_DAY) {
+                hour = !afterMidday ? 0 : 12;
+            } else if (afterMidday) {
+                hour += HALF_OF_DAY;
+            }
+            break;
+        }
+        case '12h':
+            console.warn('It is not 100% accurate to parse 12h format. Please, use AM-PM time format instead');
+            break;
+        default:
+            break;
+    }
+
+    return hour + Number.parseFloat((minutes / MINUTES_IN_HOUR).toFixed(2));
+};
+
+export { parseTime };
 export default formatTime;
